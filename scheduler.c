da340d7f9debe91538b535e0e08e1f97895cde53
dinamaged1 <76622624+dinamaged1@users.noreply.github.com>
2022-05-04 22:37:59 +0200

    updated scheduler to decide the algorithm

--- scheduler.c
+++ scheduler.c
@@ -1,5 +1,9 @@
 #include "headers.h"
 
+void SJF();
+void HPF();
+void RR(int quantum);
+void MLFQ();
 int main(int argc, char *argv[])
 {
     int AlgoType = -1;
@@ -8,6 +12,31 @@ int main(int argc, char *argv[])
     // TODO: implement the scheduler.
     receiveInt(&AlgoType);
     receiveInt(&quantum);
+    switch (AlgoType)
+    {
+    case 1:
+        SJF();
+        break;
+    case 2:
+        HPF();
+        break;
+    case 3:
+        RR(quantum);
+        break;
+    case 4:
+        MLFQ();
+        break;
+    default:
+        printf("Invalid Algorithm Type\n");
+        break;
+    }
+    // TODO: upon termination release the clock resources.
+    destroyClk(true);
+}
+
+//shortest job first algorithm
+void SJF()
+{
     int pGeneratorToSchedulerQueue = msgget(1234, 0666 | IPC_CREAT);
     if (pGeneratorToSchedulerQueue == -1)
     {
@@ -17,10 +46,24 @@ int main(int argc, char *argv[])
     struct processMsgBuff message;
     while (1)
     {
-        rec_process = msgrcv(pGeneratorToSchedulerQueue, &message, sizeof(message.mtext), 1, !IPC_NOWAIT);
+        rec_process = msgrcv(pGeneratorToSchedulerQueue, &message, sizeof(message.process), 1, !IPC_NOWAIT);
+        if (rec_process == -1)
+            perror("Error in receive");
+        else
+        {
+            printf("\nMessage received: %s\n", message.mtext);
+            struct Process p = message.process;
+            int pid = fork();
+            if (pid == 0)
+            {
+                //child process
+            }
+            else
+            {
+                //parent process
+
+            }
+        }
 
     }
-    // TODO: upon termination release the clock resources.
-
-    destroyClk(true);
 }
