bea402a26c60079e78e8135722d18bfa62dfad40
amrzakii2000 <amrz@silverkeytech.com>
2022-05-07 19:52:33 +0200

    Added round robin

--- scheduler.c
+++ scheduler.c
@@ -5,6 +5,7 @@ void SJF();
 void HPF();
 void RR(int quantum);
 void MLFQ();
+void alarmHandler();
 
 struct Queue *processesQueue;
 
@@ -76,16 +77,82 @@ void HPF()
 
 void RR(int quantum)
 {
-    while(1)
+    int rec_process = 0;
+    struct Process *temp = NULL;
+    int c = 0;
+    int pGeneratorToSchedulerQueue = msgget(1234, 0666 | IPC_CREAT);
+    if (pGeneratorToSchedulerQueue == -1)
     {
-        int x = getClk();
-        printf("\nCurrent time: %d\n", x);
+        perror("Error in create");
+        exit(-1);
+    }
+    struct Queue *q = createQueue();
+    struct processMsgBuff message;
+
+    while (!rec_process)
+    {
+        rec_process = msgrcv(pGeneratorToSchedulerQueue, &message, sizeof(message.process), 1, IPC_NOWAIT);
+        if (rec_process == -1)
+            printf("Error in receive");
+        else
+        {
+            rec_process = 0;
+            printf("\nMessage received: %d\n", message.process.id);
+            enqueue(q, &message.process);
+            printf("\nQueue front: %d\n  Queue rear %d \n ", q->front->id, q->rear->id);
+        }
+    }
+    while (1)
+    {
+        while (!isEmpty(q))
+        {
+            q->front->remainingTime -= 1;
+            printf("\nProcess %d remaining time: %d\n", q->front->id, q->front->remainingTime);
+            sleep(1);
+            c++;
+            if (c == quantum)
+            {
+                if ((q->front->remainingTime) != 0)
+                {
+                    temp = dequeue(q);
+                    while (!rec_process)
+                    {
+                        rec_process = msgrcv(pGeneratorToSchedulerQueue, &message, sizeof(message.process), 1, IPC_NOWAIT);
+                        printf("\nMessage received: %d\n", message.process.id);
+                        enqueue(q, &message.process);
+                    }
+                    enqueue(q, temp);
+                }
+                else
+                {
+                    temp = dequeue(q);
+                    printf("\nProcess %d finished\n", temp->id);
+                }
+                c = 0;
+            }
+            while (!rec_process)
+            {
+                rec_process = msgrcv(pGeneratorToSchedulerQueue, &message, sizeof(message.process), 1, IPC_NOWAIT);
+                printf("\nMessage received: %d\n", message.process.id);
+                enqueue(q, &message.process);
+            }
+        }
+        while (!rec_process)
+        {
+            rec_process = msgrcv(pGeneratorToSchedulerQueue, &message, sizeof(message.process), 1, IPC_NOWAIT);
+            printf("\nMessage received: %d\n", message.process.id);
+            enqueue(q, &message.process);
+        }
     }
 }
 
 void MLFQ()
 {
 
+}
+
+void alarmHandler()
+{
 }
 
 void clearResources(int signum)
