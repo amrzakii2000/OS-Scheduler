4c535f8e1fca9d0490812e4eeade3d941bdf3b24
AbdelrhmanAmrAli <76621761+AbdelrhmanAmrAli@users.noreply.github.com>
2022-05-18 04:17:33 +0200

    Update scheduler.c

--- scheduler.c
+++ scheduler.c
@@ -8,7 +8,6 @@
 #define MULTILEVEL_FEEDBACK_QUEUE 4
 #define MAXMEMSIZE 1024
 
-
 void clearResources(int);
 void SJF();
 void HPF();
@@ -26,7 +25,6 @@ void recieveMultiLevelProcesses();
 void countIdleClockCycles();
 void countClockCycles();
 bool checkMemory(struct Process *p);
-
 
 int AlgoType;
 int quantum;
@@ -53,21 +51,20 @@ struct Queue *diskQueue;
 FILE *memoryLog;
 bool memory[MAXMEMSIZE] = {false};
 
-
 int main(int argc, char *argv[])
 {
-    //clear resources on interrupt
+    // clear resources on interrupt
     signal(SIGINT, clearResources);
 
-    //Set recieved all processes flag to true
+    // Set recieved all processes flag to true
     signal(SIGUSR1, handler);
 
-    //Handle the signal sent by finished process
+    // Handle the signal sent by finished process
     signal(SIGUSR2, childHandler);
 
     initClk();
 
-    //read command line arguments 
+    // read command line arguments
     AlgoType = atoi(argv[1]);
     quantum = atoi(argv[2]);
     processesCount = atoi(argv[3]);
@@ -95,6 +92,7 @@ int main(int argc, char *argv[])
 
     fprintf(schedulerLog, "At time %d all processes finished\n", getClk());
     fclose(schedulerLog);
+    fclose(memoryLog);
 
     schedulerPerf = fopen("scheduler.perf", "w");
     fprintf(schedulerPerf, "CPU Utilization: %.2f %%\n", ((float)(totalClockCycles - idleClockCycles) / totalClockCycles) * 100);
@@ -102,9 +100,9 @@ int main(int argc, char *argv[])
     fprintf(schedulerPerf, "Average WTA: %.2f\n", totalWeightedTurnAroundTime / processesCount);
     fclose(schedulerPerf);
 
-    //Raise interrupt signal to clear all resources
+    // Raise interrupt signal to clear all resources
     raise(SIGINT);
-    
+
     return 0;
 }
 
@@ -114,8 +112,8 @@ void SJF()
     fprintf(schedulerLog, "----------          SJF algorithm started          ---------\n");
     fprintf(schedulerLog, "At time x process y state arr w total z remain y wait k\n\n");
     processesQueue = createQueue();
-    
-    //Intialize IPC
+
+    // Intialize IPC
     pGeneratorToSchedulerQueue = msgget(1234, 0666 | IPC_CREAT);
 
     if (pGeneratorToSchedulerQueue == -1)
@@ -124,10 +122,10 @@ void SJF()
     }
     while (1)
     {
-        //Recieve processes sent by process generator
+        // Recieve processes sent by process generator
         recieveProcess();
-        
-        //Get a process ready for running if no other process is running and the queue is not empty
+
+        // Get a process ready for running if no other process is running and the queue is not empty
         if (!currentRunning && !isEmpty(processesQueue))
         {
             processSend = dequeue(processesQueue);
@@ -136,7 +134,7 @@ void SJF()
             fprintf(schedulerLog, "At time %d process %d %s arr %d total %d remain %d wait %d\n", getClk(), processSend->id, getProcessStateText(processSend->state), processSend->arrivalTime, processSend->runTime, processSend->remainingTime, processSend->waitTime);
             processSend->remainingTime = 0;
 
-            //Fork the running process
+            // Fork the running process
             runningProcessPid = fork();
             if (runningProcessPid == 0)
             {
@@ -149,13 +147,13 @@ void SJF()
                 currentRunning = true;
             }
         }
-        //Break from the schduler when all processes are finished 
+        // Break from the schduler when all processes are finished
         if (recivedAllProcesses && isEmpty(processesQueue) && !currentRunning)
         {
             break;
         }
 
-        //Count total cycles and idle clock cycles
+        // Count total cycles and idle clock cycles
         countClockCycles();
     }
 }
@@ -173,7 +171,7 @@ void HPF()
     }
     while (1)
     {
-        //Function used to recieve process for HPF algorithm
+        // Function used to recieve process for HPF algorithm
         recieveProcessHPF();
         if (!currentRunning && !isEmpty(processesQueue))
         {
@@ -184,12 +182,12 @@ void HPF()
             processSend->state = STARTED;
 
             fprintf(schedulerLog, "At time %d process %d %s arr %d total %d remain %d wait %d\n", getClk(), processSend->id, getProcessStateText(processSend->state), processSend->arrivalTime, processSend->runTime, processSend->remainingTime, processSend->waitTime);
-            
-            //Used to store older pre-empted process remaining time
+
+            // Used to store older pre-empted process remaining time
             runningProcessRemainingTime = processSend->remainingTime;
             processSend->remainingTime = 0;
 
-            //If the process has not been forked before
+            // If the process has not been forked before
             if (processSend->pid == -1)
             {
                 runningProcessPid = fork();
@@ -206,7 +204,7 @@ void HPF()
             }
             else
             {
-                //If the process has been forked before and it is its turn to run, we send continue signal
+                // If the process has been forked before and it is its turn to run, we send continue signal
                 processSend->state = RESUMED;
                 kill(processSend->pid, SIGCONT);
                 runningProcessPid = processSend->pid;
@@ -234,10 +232,10 @@ void RR(int quantum)
     }
     while (1)
     {
-        //recieve processes sent
+        // recieve processes sent
         recieveProcess();
 
-        //Handle the stopping of the running process if it finished its quantum
+        // Handle the stopping of the running process if it finished its quantum
         stoppingHandler();
         if (!currentRunning && !isEmpty(processesQueue))
         {
@@ -248,8 +246,8 @@ void RR(int quantum)
             processSend->waitTime += getClk() - processSend->stoppingTime;
 
             runningProcessRemainingTime = processSend->remainingTime;
-            
-            //Calculate process remaining time
+
+            // Calculate process remaining time
             if (processSend->remainingTime >= quantum)
                 processSend->remainingTime -= quantum;
             else
@@ -274,7 +272,7 @@ void RR(int quantum)
             }
             else
             {
-                //Continue the process if it is forked before
+                // Continue the process if it is forked before
                 processSend->state = RESUMED;
                 kill(processSend->pid, SIGCONT);
                 runningProcessPid = processSend->pid;
@@ -300,13 +298,13 @@ void MLFQ(int quantum)
         perror("Error in create");
     }
 
-    //Memory allocation for multilievel feedback queue
+    // Memory allocation for multilievel feedback queue
     InitializeMultiLevelQueue();
     while (1)
     {
-        //Recive arriving process in its corressponding level
+        // Recive arriving process in its corressponding level
         recieveMultiLevelProcesses();
-        //Handle process stopping
+        // Handle process stopping
         stoppingHandler();
         processesQueue = getPriorityQueue(multiLevelQueue, PRIORITY_LEVELS);
 
@@ -438,8 +436,8 @@ void recieveProcessHPF()
         {
 
             p = createProcess(message.process.id, message.process.priority, message.process.runTime, message.process.arrivalTime);
-            
-            //If a process with higher priority arrives, interrupt the current running process
+
+            // If a process with higher priority arrives, interrupt the current running process
             if (processSend && p->priority < processSend->priority)
             {
                 processSend->remainingTime = runningProcessRemainingTime - getClk() + processSend->startTime;
@@ -473,8 +471,8 @@ void recieveMultiLevelProcesses()
             p = createProcess(message.process.id, message.process.priority, message.process.runTime, message.process.arrivalTime);
             enqueue(multiLevelQueue[p->priority], p);
             fprintf(schedulerLog, "At time %d process %d %s arr %d total %d remain %d wait %d\n", getClk(), p->id, getProcessStateText(p->state), p->arrivalTime, p->runTime, p->remainingTime, p->waitTime);
-            
-            //Check if a process of higher priority arrived
+
+            // Check if a process of higher priority arrived
             if (currentRunning && processSend && p->priority < processSend->priority)
             {
                 processSend->remainingTime = runningProcessRemainingTime - getClk() + processSend->startTime;
@@ -499,7 +497,7 @@ void InitializeMultiLevelQueue()
     }
 }
 
-//Function used to check if current running process finished its quantum
+// Function used to check if current running process finished its quantum
 void stoppingHandler()
 {
     if (currentRunning && processSend->remainingTime != 0 && getClk() - processSend->startTime == quantum)
@@ -510,7 +508,7 @@ void stoppingHandler()
         processSend->stoppingTime = getClk();
         fprintf(schedulerLog, "At time %d process %d %s arr %d total %d remain %d wait %d\n", getClk(), processSend->id, getProcessStateText(processSend->state), processSend->arrivalTime, processSend->runTime, processSend->remainingTime, processSend->waitTime);
 
-        //In case of multi level feedback queue, we move the process to the next level
+        // In case of multi level feedback queue, we move the process to the next level
         if (AlgoType == MULTILEVEL_FEEDBACK_QUEUE)
         {
             recieveMultiLevelProcesses();
@@ -525,7 +523,7 @@ void stoppingHandler()
     }
 }
 
-//Used to count clock cycles
+// Used to count clock cycles
 void countClockCycles()
 {
     if (currentClock < getClk())
